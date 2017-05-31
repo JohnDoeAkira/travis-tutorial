4c5332d73a428665b30430e162f2c63d640b060c
JohnDoeAkira <akira.johndoe@gmail.com>
2017-05-31 12:07:03 +0200

    Update test_all.py

--- test_all.py
+++ test_all.py
@@ -1,3 +1,6 @@
+import numpy
+import numpy.random
+
 def test_addition():
     assert 1 + 1 == 2
     
@@ -6,3 +9,11 @@ def test_True():
     
 def test_division():
     assert 3/2 == 1.5
+
+def test_partition():
+    array_test = numpy.random.randint(20, size=10)
+    k_ind = 10
+    array_part = numpy.partition(array_test, 10)
+    
+    for v in array_part[:k_ind]:
+        assert v < array_test[k_ind]
