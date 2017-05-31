af9e5bed584dfe69eae62735a69bbb8c05936112
JohnDoeAkira <akira.johndoe@gmail.com>
2017-05-31 12:09:35 +0200

    Update test_all.py

--- test_all.py
+++ test_all.py
@@ -8,12 +8,15 @@ def test_True():
     assert 1 == 1
     
 def test_division():
+    print(numpy.__version__)
     assert 3/2 == 1.5
-
+  
 def test_partition():
     array_test = numpy.random.randint(20, size=10)
-    k_ind = 10
-    array_part = numpy.partition(array_test, 10)
+    k_ind = 5
+    array_part = numpy.partition(array_test, k_ind)
     
     for v in array_part[:k_ind]:
         assert v < array_test[k_ind]
+        
+       
